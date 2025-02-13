c014e1e6eeb980eb37332ced43f985343b6f3817
park sang hyuk <vlog.xcode@gmail.com>
2025-02-13 15:34:54 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+import colors from "./tailwind/colors";
+
+const px0_20 = Array.from({ length: 21 }, (_, i) => `${i}px`);
+const px0_200 = Array.from({ length: 201 }, (_, i) => `${i}px`);
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderWidth: {
+        ...px0_20,
+      },
+      spacing: {
+        ...px0_200,
+      },
+      colors: colors,
+    },
+  },
+  plugins: [],
+};
