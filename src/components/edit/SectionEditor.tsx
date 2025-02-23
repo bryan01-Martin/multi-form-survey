ff15c7e959e10f85021a4be09bfc092d7a4cb569
park sang hyuk <shplove4568@gmail.com>
2025-02-23 14:15:34 +0900

    feat: Question Footer 추가

--- src/components/edit/SectionEditor.tsx
+++ src/components/edit/SectionEditor.tsx
@@ -11,7 +11,12 @@ function SectionEditor({ section }: Props) {
     <div className={"[&>*]:mb-24"}>
       <SectionTitleEditor capTitle={"2개 중 1번째"} section={section} />
       {section.questions.map((question) => (
-        <QuestionEditor key={question.id} question={question} />
+        <QuestionEditor
+          key={question.id}
+          question={question}
+          onCopy={section.copyQuestion}
+          onDelete={section.removeQuestion}
+        />
       ))}
     </div>
   );
